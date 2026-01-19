2281e64d6a91bc60234c51fd70d1b29e4ed8a2bc
AleMatGra <alexander.grath.96+github@gmail.com>
2026-01-19 19:49:11 +0100

    most of ms_3

---
+++ milestone_03/drop_3NF.sql
@@ -0,0 +1,20 @@
+-- Meilenstein 3
+-- Datenbanksysteme
+-- Gruppe: Boiko Bohdana, Alexander Grath
+-- Fahrplansystem Wien Praterstern
+
+DROP TABLE Ticket_Linie;
+DROP TABLE Ticket;
+DROP TABLE Kunde;
+DROP TABLE Stadt; -- neu
+DROP TABLE Linie_Haltestelle;
+DROP TABLE Haltestelle;
+DROP TABLE Linie;
+DROP TABLE Wartung;
+DROP TABLE Bus;
+DROP TABLE Zug;
+DROP TABLE Strassenbahn;
+DROP TABLE Fahrzeug;
+DROP TABLE Fahrer;
+DROP TABLE Kontrolleur;
+DROP TABLE Personal;
